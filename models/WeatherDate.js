bb4a707168cd3af400e62fc526678e4a681493e7
Christian Thompson <christian510@gmail.com>
2021-01-31 15:58:25 -0700

    update to date methods

--- models/WeatherDate.js
+++ models/WeatherDate.js
@@ -8,17 +8,17 @@ const date_time = {
 module.exports = class WeatherDate {
   static convertUTC(dt, sunr, suns, offset) {
     console.log(dt);
-    const displayDate = new Date(dt * 1000);
-    let dtOptions = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric'};
-    date_time.date = displayDate.toLocaleTimeString("en-US", dtOptions);
+    const displayDate = new Date(dt);
+    let dtOptions = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
+    date_time.date = displayDate.toLocaleString("en-US", dtOptions);
 
-    const sunrise = new Date(sunr * 1000);
-    let sunrOptions = { hour: 'numeric', minute: 'numeric' }
-    date_time.sunrise = sunrise.toLocaleTimeString("en-US", sunrOptions);
+    const sunrise = new Date(sunr);
+    let sunrOptions = { hour: 'numeric', minute: 'numeric', timeZone: 'UTC' }
+    date_time.sunrise = sunrise.toLocaleString("en-US", sunrOptions);
 
-    const sunset = new Date(suns * 1000);
+    const sunset = new Date(suns);
     let sunsOptions = { hour: 'numeric', minute: 'numeric' }
-    date_time.sunset = sunset.toLocaleTimeString("en-US", sunsOptions);
+    date_time.sunset = sunset.toLocaleString("en-US", sunsOptions);
     return date_time;
   }
 }
