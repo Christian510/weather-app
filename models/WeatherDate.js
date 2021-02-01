f89b076642c5c6189dbd965bad9cfacfb9d4535a
Christian Thompson <christian510@gmail.com>
2021-01-31 18:15:38 -0700

    added timezone

--- models/WeatherDate.js
+++ models/WeatherDate.js
@@ -6,18 +6,18 @@ const date_time = {
 }
 
 module.exports = class WeatherDate {
-  static convertUTC(dt, sunr, suns, offset) {
+  static convertUTC(dt, sunr, suns, offset, timezone) {
     console.log(dt);
-    const displayDate = new Date(dt);
-    let dtOptions = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC'};
+    const displayDate = new Date(dt * 1000);
+    let dtOptions = { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: timezone};
     date_time.date = displayDate.toLocaleString("en-US", dtOptions);
 
-    const sunrise = new Date(sunr);
-    let sunrOptions = { hour: 'numeric', minute: 'numeric', timeZone: 'UTC' }
+    const sunrise = new Date(sunr * 1000);
+    let sunrOptions = { hour: 'numeric', minute: 'numeric', timeZone: timezone }
     date_time.sunrise = sunrise.toLocaleString("en-US", sunrOptions);
 
-    const sunset = new Date(suns);
-    let sunsOptions = { hour: 'numeric', minute: 'numeric' }
+    const sunset = new Date(suns * 1000);
+    let sunsOptions = { hour: 'numeric', minute: 'numeric', timeZone: timezone }
     date_time.sunset = sunset.toLocaleString("en-US", sunsOptions);
     return date_time;
   }
