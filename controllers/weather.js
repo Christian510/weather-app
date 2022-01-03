74919a4d4f64e4071858e6cbceb816fd6488a206
Christian Thompson <christian510@gmail.com>
2022-01-03 12:12:06 -0700

    debug weather.js

--- controllers/weather.js
+++ controllers/weather.js
@@ -46,6 +46,7 @@ exports.postWeatherByName = (req, res, next) => {
     } else {
       WeatherData.getWeather(city.coord.lat, city.coord.lon, w => {
         let cw = w.data.current;
+				console.log(cw);
         let c = cw.clouds;
         let offset = w.data.timezone_offset;
         let timezone = w.data.timezone;
