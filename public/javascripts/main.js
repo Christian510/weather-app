b6c7d46fe9f169a399cf64f304660bc59bd4d710
Christian Thompson <christian510@gmail.com>
2022-02-08 12:38:58 -0700

    prevent defaults touchmove touchgesture

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -22,52 +22,18 @@ function savedMsg(e) {
 }
 
 window.addEventListener('DOMContentLoaded', function() {
-	let body = document.querySelector('body');
-	let scrolling = document.querySelector('.scrolling');
-	console.log(scrolling);
-	let yValue = null,
-	scollStart = false;
 	
 	// Obviously this will disable touchmove on all pages;
-	body.addEventListener('gesturechange', function(e) {
+	window.addEventListener('gesturechange', function(e) {
 		e.preventDefault();
+		console.log('touch gestures prevented.')
 	}, {passive: false});
 
-	body.addEventListener('touchmove', function(e) {
+	window.addEventListener('touchmove', function(e) {
 		e.preventDefault();
-	}, {passive: false})
-
-  scrolling.addEventListener('touchstart', function(e) {
-		// e.preventDefault();
-		if (e.targetTouches.length === 1) {
-			yValue = e.targetTouches[0].clientY;
-			// console.log(`Y value: ${e.targetTouches.length}`);
-			console.log(`Y value: ${yValue}`);
-			// console.log(`scrollTop: ${body.scrollTop}`);
-		}
-		// console.dir(`Outside scrollable area: ${e.targetTouches[0]}`);
+		console.log('toucmove prevent')
 	}, {passive: false});
 
-	scrolling.addEventListener('touchmove', function(e) {
-		// console.log(`is 1? ${e.targetTouches.length}`)
-		if(e.targetTouches.length === 1) {
-			console.log(`touchmove started: ${e.targetTouches}`)
-		}
-	}, {passive: false});
-
-		scrolling.addEventListener('touchend', function(){
-			console.log(`touch ended: ${e.targetTouches}`)
-		}, {passive: false});
-
-
-
-	// function disableRubberBand(e) {
-	// 	let stop = e.targetTouches[0].clientY - yValue;
-	// 	console.log(`stop: ${stop}`);
-	// 	if (scroll.scrollTop === 0 && stop > 0) {
-	// 		e.preventDefault();
-	// 	}
-	// }
 
   
 })
