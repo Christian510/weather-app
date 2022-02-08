62eea2f1ec929f157df43c886835a59a0a9ce199
Christian Thompson <christian510@gmail.com>
2022-02-08 12:18:03 -0700

    css touch action

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -69,26 +69,13 @@ window.addEventListener('DOMContentLoaded', function() {
 	// 	}
 	// }
 
-// 	const scroll = document.querySelector('.scrollable-list');
-// 	scroll.addEventListener('touchstart', function(e) {
-// 		console.dir(`Inside scrollable area: ${e}`);
-// 	})
-// 	scroll.addEventListener('touchmove', function(e) {
-// 		e.preventDefault();
-// 	}, false);
-// 	scroll.addEventListener('touchend', function(e) {
-// 		e.preventDefault();
-// 	}, false);
-
   
 })
 
-  // const scroll = document.querySelector('#scroll');
-  // scroll.addEventListener('scroll', function(e) {
-  //   console.log(window.scrollY);
-  // });
-  // https://stackoverflow.com/questions/41594997/ios-10-safari-prevent-scrolling-behind-a-fixed-overlay-and-maintain-scroll-posi
-
-// 1. Write a function that deletes an item and updates the list pull in WeatherData and use it to make the changes to the db Then update the list //
-
-// 2. Write a function that activeates a modal to warn user of incomplete search.  Search requires a valid city and state //
+	
+	/* Dev Notes:
+	1. use touch events that allows for scrolling on pages with scrollable content.  But, prevents the how body of the page on ios from being dragged around.
+	* I believe this has to be associated with the css and how the css moves div assocaited with the scrollable content.
+		resrouces:
+		https://stackoverflow.com/questions/41594997/ios-10-safari-prevent-scrolling-behind-a-fixed-overlay-and-maintain-scroll-posi
+	*/
