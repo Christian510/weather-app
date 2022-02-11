44115501eb1753105bdeab5bc84eb933fba14dd5
Christian Thompson <christian510@gmail.com>
2022-02-11 14:24:33 -0700

    Scrolling works.  Need to get offset dialed in.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -24,18 +24,63 @@ function savedMsg(e) {
 // ----- TOUCH EVENTS ------ //
 window.addEventListener('DOMContentLoaded', function() {
 	const preventDefault = e => { e.preventDefault() }
+	const consoleFunc = e => { console.log(e.touches)};
+	const container = document.querySelector('.scrollable-area')
 
-	window.addEventListener('gesturechange', function(e) {
-		e.preventDefault();
-		console.log('touch gestures prevented.')
-	}, {passive: false});
+	let elm = document.querySelector('.scrollable-area');
+	console.log("scroll height: ", elm.scrollHeight);
 
-	window.addEventListener('touchmove', function(e) {
-		console.log(`touches ${e.targetTouches}`);
-	}, {passive: false})
+	window.addEventListener('gesturechange', preventDefault, {passive: false});
+	// window.addEventListener('touchstart', touchStart, {passive: false})
+	// window.addEventListener('touchmove', preventDefault, {passive: false});
+	// window.addEventListener('touchstart', scrollArea, {passive: false})
   
-})
+	container.addEventListener('touchstart', touchStart, {passive: false});
+	container.addEventListener('touchmove', touchmove, {passive: false});
+	container.addEventListener('touchend', touchEnd, {passive: false});
 
+	let scrolling = false;
+	let curY = null;
+	let offSet = container.offsetHeight
+
+	function touchStart(e) {
+		if (e.targetTouches.length === 1) {
+			console.log`touches: ${e.touches} | offsetHeight: ${offSet}`;
+			curY = e.touches[0].pageY;
+			console.log(`start point: ${curY}`)
+			scrolling = true;
+		}
+	}
+
+	function touchmove(e) {
+		if (e.targetTouches.length === 1) {
+			console.log`${e.touches}`;
+			for (var i=0; i < e.changedTouches.length; i++) {
+				let scrolling = e.changedTouches[i].clientY;
+				console.log(`scrolling: ${scrolling}`);
+				container.style.transform = `translateY(${scrolling}px)`;
+			}
+		}
+	}
+
+	function touchEnd(e) {
+		console.log(e.targetTouches);
+	}
+	
+	function scrollArea(e) {
+		let listHeight = container.firstElementChild.offsetHeight
+		// console.log("scrollHeight: ", container.scrollHeight)
+		// console.log(container.offsetHeight)
+		// console.log(e.touches)
+		// console.log(container.firstElementChild.offsetHeight)
+		// console.log(window.visualViewport)
+		// console.log(`list hight: ${listHeight} | ${e.touches[0].clientY} | ${e.changedTouches[0]}`);
+		for (var i=0; i < e.changedTouches.length; i++) {
+			console.log("changedTouches[" + i + "].clientY = " + e.changedTouches[i].clientY);
+		}
+	}
+	
+})
 if (window.location.pathname !== '/') {
 	window.removeEventListener('gesturechange', preventDefault);
 	window.removeEventListener('touchmove', preventDefault);
